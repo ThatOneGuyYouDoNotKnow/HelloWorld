51b6c02ac1ccc238487895548235bbb9e7c81c1b
ThatOneGuyYouDoNotKnow <rene.heidrich-privat@outlook.de>
2018-09-19 09:35:05 +0200

    defined more expected behavior of PersistanceService.Save

--- WpfApp/PersistenceTests/PersistenceServiceTests/SaveTests.cs
+++ WpfApp/PersistenceTests/PersistenceServiceTests/SaveTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Persistence;
 
@@ -15,6 +17,39 @@ namespace PersistenceTests.PersistenceServiceTests
 
             //assert
             Assert.ThrowsException<ArgumentException>(() => persistenceService.Save(new object(), "invalidPath"));
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void SerializableObjectAtInvalidPath_ArgumentException()
+        {
+            //arrange
+            PersistenceService persistenceService = new PersistenceService();
+
+            //assert
+            Assert.ThrowsException<ArgumentException>(() => persistenceService.Save(new SerializableObject(), "invalidPath"));
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void SerializableObjectAtValidPath_FileCreated()
+        {
+            //arrange
+            const string pathToSaveTo = "validPath"; //todo: set an actually valid path
+            PersistenceService persistenceService = new PersistenceService();
+
+            //act
+            persistenceService.Save(new SerializableObject(), pathToSaveTo);
+
+            //assert
+            Assert.IsTrue(File.Exists(pathToSaveTo));
+        }
+
+        [DataContract]
+        private class SerializableObject
+        {
+            [DataMember]
+            public string TestString { get; set; }
         }
     }
 }
