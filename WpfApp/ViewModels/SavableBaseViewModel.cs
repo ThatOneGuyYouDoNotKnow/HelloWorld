6dc3d762768eca4789d6e1448561b1149eeb13ed
ThatOneGuyYouDoNotKnow <rene.heidrich-privat@outlook.de>
2018-09-19 08:14:11 +0200

    created SavableBaseViewModel

---
+++ WpfApp/ViewModels/SavableBaseViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+using JetBrains.Annotations;
+
+namespace ViewModels
+{
+    public abstract class SavableBaseViewModel<T> : BaseViewModel
+    {
+        protected SavableBaseViewModel()
+        {
+            SaveCommand = new RelayCommand(Save);
+            LoadCommand = new RelayCommand(Load);
+        }
+
+        [NotNull]
+        public ICommand SaveCommand { get; }
+
+        [NotNull]
+        public ICommand LoadCommand { get; }
+
+        public string SaveLocation { get; set; }
+
+        private void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
